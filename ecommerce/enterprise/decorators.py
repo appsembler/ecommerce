84dc2445d2f3896fe4fa642d7944f9109cc7d664
Douglas Hall <douglashall@dhcp-18-189-82-79.dyn.mit.edu>
2017-07-17 15:13:33 -0400

    Set max_age on Enterprise cookie.
    We set a cookie which identifies a user as being associated with
    a given enterprise based on how they navigated to the Open edX site.
    This cookie gets shared across edX services, so that the enterprise
    context can be maintained when navigating between UIs across these
    services. This cookie needs to be short-lived so that the Enterprise
    context does not linger longer than it should. This change sets a
    60 second max age on the Enterprise cookie.
    
    ENT-367

--- ecommerce/enterprise/decorators.py
+++ ecommerce/enterprise/decorators.py
@@ -10,21 +10,32 @@ from ecommerce.enterprise import utils
 Voucher = get_model('voucher', 'Voucher')
 
 
-def set_enterprise_cookie(func):
+def set_enterprise_cookie(func, max_age=None):
     """
     Decorator for applying cookie with enterprise customer uuid.
+
+    Arguments:
+        func (function): The function to decorate.
+        max_age (int): The max_age to set on the enterprise cookie (seconds).
+
+    Returns:
+        function: The decorated function.
     """
     @wraps(func)
     def _decorated(request, *args, **kwargs):
-        code, enterprise_customer_uuid = request.GET.get('code'), None
-        if code:
-            enterprise_customer_uuid = utils.get_enterprise_customer_uuid(code)
-
         response = func(request, *args, **kwargs)
 
         # Set enterprise customer cookie if enterprise customer uuid is available.
-        if enterprise_customer_uuid:
-            response = utils.set_enterprise_customer_cookie(request.site, response, enterprise_customer_uuid)
+        code, enterprise_customer_uuid = request.GET.get('code'), None
+        if code:
+            enterprise_customer_uuid = utils.get_enterprise_customer_uuid(code)
+            if enterprise_customer_uuid:
+                response = utils.set_enterprise_customer_cookie(
+                    request.site,
+                    response,
+                    enterprise_customer_uuid,
+                    max_age=max_age
+                )
 
         return response
     return _decorated
